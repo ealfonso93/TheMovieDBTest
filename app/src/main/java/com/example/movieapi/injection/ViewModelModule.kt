9702962d9e059004a598600f851cf0c3f73a6685
rufo <ealfonso93@gmail.com>
2019-11-30 17:43:50 +0100

    fix module annotation

--- app/src/main/java/com/example/movieapi/injection/ViewModelModule.kt
+++ app/src/main/java/com/example/movieapi/injection/ViewModelModule.kt
@@ -3,8 +3,10 @@ package com.example.movieapi.injection
 import androidx.lifecycle.ViewModel
 import com.example.movieapi.ui.main.MainViewModel
 import dagger.Binds
+import dagger.Module
 import dagger.multibindings.IntoMap
 
+@Module
 internal abstract class ViewModelModule {
 
     @Binds
