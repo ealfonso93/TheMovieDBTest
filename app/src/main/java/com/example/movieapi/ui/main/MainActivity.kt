e831a43c5a73c8862ff7a4b9a55fe4b1f69155b5
rufo <ealfonso93@gmail.com>
2019-11-30 21:55:51 +0100

    finish implementing list

--- app/src/main/java/com/example/movieapi/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/movieapi/ui/main/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.movieapi.ui.main
 
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.movieapi.R
 import com.example.movieapi.base.ViewModelActivity
 import com.example.movieapi.databinding.MainActivityBinding
+import com.example.movieapi.models.Show
+import com.example.movieapi.ui.main.list.PopularShowViewHolder
+import com.example.movieapi.ui.main.list.PopularShowsListAdapter
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity: ViewModelActivity(){
+class MainActivity: ViewModelActivity(), PopularShowViewHolder.OnClickListener{
+
 
     private val vm by viewModel<MainViewModel>()
     private val binding by binding<MainActivityBinding>(R.layout.main_activity)
@@ -16,6 +22,12 @@ class MainActivity: ViewModelActivity(){
             lifecycleOwner = this@MainActivity
             viewModel = vm
         }
+
+        recyclerView.adapter = PopularShowsListAdapter(this)
+        recyclerView.layoutManager = GridLayoutManager(applicationContext, 3)
     }
 
+    override fun onItemClick(show: Show) {
+        //Do nothing
+    }
 }
