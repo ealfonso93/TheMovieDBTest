e42b4c5a8f274b2b9c41c36417ed2c1dbcc375af
rufo <ealfonso93@gmail.com>
2019-11-30 11:30:21 +0100

    add movi api service

---
+++ app/src/main/java/com/example/movieapi/network/MovieService.kt
@@ -0,0 +1,11 @@
+package com.example.movieapi.network
+
+import androidx.lifecycle.LiveData
+import com.example.movieapi.models.PopularShowResponse
+import retrofit2.http.GET
+
+interface MovieService {
+
+    @GET("//tv/popular?language=en")
+    fun fetchPopularShows(): LiveData<ApiResponse<PopularShowResponse>>
+}
