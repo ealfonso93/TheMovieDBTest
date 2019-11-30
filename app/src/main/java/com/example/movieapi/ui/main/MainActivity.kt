bb7e6190d4c22ac22cc47f3c150e4bf7dca72b0f
rufo <ealfonso93@gmail.com>
2019-11-30 20:12:47 +0100

    remove fragment and setup MainView

--- app/src/main/java/com/example/movieapi/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/movieapi/ui/main/MainActivity.kt
@@ -2,18 +2,16 @@ package com.example.movieapi.ui.main
 
 import android.os.Bundle
 import com.example.movieapi.R
+import com.example.movieapi.base.ViewModelActivity
 import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : DaggerAppCompatActivity(){
+class MainActivity: ViewModelActivity(){
+
+    private val vm by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
     }
 
 }
--- app/src/main/java/com/example/movieapi/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/movieapi/ui/main/MainViewModel.kt
@@ -1,7 +1,12 @@
 package com.example.movieapi.ui.main
 
 import androidx.lifecycle.ViewModel
+import com.example.movieapi.repository.MovieRepository
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel  @Inject
+constructor(
+    private val movieRepository: MovieRepository
+) : ViewModel() {
+// TODO: Implement the ViewModel
 }
