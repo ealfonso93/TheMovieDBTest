306d03683505d0b1b2086800695ccb4ab8f521e0
rufo <ealfonso93@gmail.com>
2019-11-30 21:54:38 +0100

    implement viewmodel

--- app/src/main/java/com/example/movieapi/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/movieapi/ui/main/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.movieapi.ui.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.movieapi.models.Show
 import com.example.movieapi.repository.MovieRepository
 import javax.inject.Inject
 
@@ -8,5 +10,10 @@ class MainViewModel  @Inject
 constructor(
     private val movieRepository: MovieRepository
 ) : ViewModel() {
-// TODO: Implement the ViewModel
+
+    val showsListLiveData: LiveData<List<Show>>
+
+    init {
+        showsListLiveData = movieRepository.loadPopularShows()
+    }
 }
